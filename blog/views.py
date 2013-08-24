e1feff1ba3589bb972ca1a50e09278e0199ba7e9
伊藤結 <yuiitoh@itou-no-MacBook-Air.local>
2013-08-24 17:46:08 +0900

    CSRFに対応

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.template import Context, loader, RequestContext
+from django.views.decorators.csrf import csrf_protect
 from models import Content
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render_to_response, get_object_or_404
@@ -9,11 +10,12 @@ from django.core.urlresolvers import reverse
 def index(request):
     content_list = Content.objects.all().order_by('id').reverse()
     t = loader.get_template('blog/index.html')
-    c = Context({
+    c = RequestContext(request, {
             'content_list':content_list,
     })
     return HttpResponse(t.render(c))
 
+@csrf_protect
 def write(request):
     try:
         b = Content(title=request.POST['title'], \
